205d2f8e44314d9fc2f7bb1f3bb96d1bfb19b09d
mesutBE <gelukkig.zijn.be@gmail.com>
2020-02-29 19:10:10 +0100

    handlers

--- handlers/reset.js
+++ handlers/reset.js
@@ -1,8 +1,15 @@
 function resetHandler() {
   debugger;
   // reset state to initial values
+  numbers.current = numbersInit.current;
+  numbers.biggest = numbersInit.biggest;
+  numbers.smallest = numbersInit.smallest;
+  numbers.all = numbersInit.all;
 
   // re-render UI with values saved in state
+  document.getElementById("input").value = numbers.current;
+  document.getElementById("biggest").innerHTML = numbers.biggest;
+  document.getElementById("smallest").innerHTML = numbers.smallest;
 
   // log interaction: handler name, new state
   log.push({
--- handlers/save-number.js
+++ handlers/save-number.js
@@ -1,14 +1,21 @@
 function saveNumberHandler() {
   debugger;
   // read new number from user input
+  let newInput = Number(document.getElementById("input").value);
 
   // read from state the data you will need for the next step
-
+  let lastValues = ''; // I plan to read but I couldn't understand why I need??
   // find the new biggest and smallest values
 
   // update state: new biggest, new smallest, new current & save the last current
+  numbers.current = newInput;
+  numbers.biggest = numbers.biggest > newInput ? numbers.biggest : newInput;
+  numbers.smallest = numbers.smallest < newInput ? numbers.smallest : newInput;
+  numbers.all.push([numbers.current, numbers.biggest, numbers.smallest]);
 
   // re-render the user interface with values stored in state
+  document.getElementById("biggest").innerHTML = numbers.biggest;
+  document.getElementById("smallest").innerHTML = numbers.smallest;
 
   // log user interaction: handler name, user input, new state
   log.push({
